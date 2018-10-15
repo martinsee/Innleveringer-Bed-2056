528f5030b660c2eb8dc8f29b4971816c78b21545
Martin Bjørnerem <martinsbjornerem@gmail.com>
2018-10-15 21:43:57 +0200

    Fixed format file

---
+++ Assignment5attempt.R
@@ -0,0 +1,8 @@
+install.packages("readr")
+library(readr)
+
+sex <- read.fwf("ftp://ftp.cdc.gov/pub/Health_Statistics/NCHS/Datasets/DVS/natality/Nat2017us.zip", widths = c(-475,1))
+head(sex)
+#Får bare en masse warnings: Line xxx appears to contain embedded null.
+
+
